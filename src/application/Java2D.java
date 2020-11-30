c19caf6d282d52d9f97f531793a0ea3cbb2b5bed
DavidsJulio <davidsjulio97@gmail.com>
2020-11-30 15:05:41 +0000

    Window creation

--- src/application/Java2D.java
+++ src/application/Java2D.java
@@ -1,12 +1,46 @@
 package application;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
-public class Java2D {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Java2D extends JFrame{
 
 	public static void main(String[] args) {
 		
 		
+		JFrame frame = new Java2D();
+		frame.setTitle("Java 2D - David Júlio");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		MyPanel panel = new MyPanel();
+		frame.getContentPane().add(panel);
+		
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setVisible(true);
 	}
 
+}
+
+class MyPanel extends JPanel{
+
+	int panelWidth = 600;
+	int panelHeight = 400;
+	
+	public MyPanel() {
+		setPreferredSize( new Dimension( panelWidth, panelHeight ) ); 
+		setBackground(Color.LIGHT_GRAY); 
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+	
+	}
 }
