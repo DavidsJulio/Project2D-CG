a0844b5508ede26b9c22cb849fbf21c489f99e4f
DavidsJulio <davidsjulio97@gmail.com>
2020-11-30 15:17:29 +0000

    Player Creation

--- src/application/Java2D.java
+++ src/application/Java2D.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -33,6 +36,15 @@ class MyPanel extends JPanel{
 	int panelWidth = 600;
 	int panelHeight = 400;
 	
+	AffineTransform at = new AffineTransform();
+	
+	//Player - Using ellipse
+	int radius = 15;
+	Shape player;
+	int translationX = 0 + 2*radius;
+	int translationY = panelHeight - 2*radius;
+	
+	
 	public MyPanel() {
 		setPreferredSize( new Dimension( panelWidth, panelHeight ) ); 
 		setBackground(Color.LIGHT_GRAY); 
@@ -41,6 +53,15 @@ class MyPanel extends JPanel{
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
+
+		
+		player = new Ellipse2D.Double(-radius, -radius, 2*radius, 2*radius);
+		at.setToTranslation(translationX, translationY);
+		player = at.createTransformedShape(player);
+		g2.setColor(Color.RED);
+		g2.fill(player);
 	
+		
 	}
+	
 }
