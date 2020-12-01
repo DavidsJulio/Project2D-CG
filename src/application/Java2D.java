115a290ab911b7ea040a17ac2ef7e761d180f223
DavidsJulio <davidsjulio97@gmail.com>
2020-12-01 11:56:57 +0000

    Implementing Runnuble and KeyListener, Player movement

--- src/application/Java2D.java
+++ src/application/Java2D.java
@@ -6,6 +6,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.Shape;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
@@ -34,7 +36,7 @@ public class Java2D extends JFrame{
 
 }
 
-class MyPanel extends JPanel{
+class MyPanel extends JPanel implements Runnable, KeyListener{
 
 	int panelWidth = 600;
 	int panelHeight = 400;
@@ -44,35 +46,37 @@ class MyPanel extends JPanel{
 	AffineTransform at = new AffineTransform();
 	
 	//Player - Using ellipse
-	Shape player;
+	Shape player = null;
 	int translationX = scale * 2;
 	int translationY = panelHeight - scale * 2;
-	
-	//Star 
-	Shape star1 = new Star( -scale, -scale, scale * 2, scale * 2 );
+	int vxPlayer = 0;
+	int vyPlayer = 0;
 	
 	//Obstacles
-	Shape wall_1 = new Rectangle2D.Double(-scale * 2, -scale * 6, scale * 4, scale * 12);
-	Shape wall_2 = new Rectangle2D.Double(-scale * 2, -scale * 4, scale * 4, scale * 8); //edit
-
+	Shape wall_1 = null;
+	Shape wall_2 = null;
+	
+	//Star 
+	Shape star1 = null;
 	
 	public MyPanel() {
 		setPreferredSize( new Dimension( panelWidth, panelHeight ) ); 
 		setBackground(Color.LIGHT_GRAY); 
+		
+		this.addKeyListener(this);
+		this.setFocusable(true);
+		
+		Thread thread = new Thread(this);
+		thread.start();
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-
-		//player
-		player = new Ellipse2D.Double( -scale, -scale, 2 * scale, 2 * scale );
-		at.setToTranslation( translationX, translationY );
-		player = at.createTransformedShape( player );
-		g2.setColor( Color.RED );
-		g2.fill( player );
 		
 		//Star
+		star1 = new Star( -scale, -scale, scale * 2, scale * 2 );
+		
 		at.setToTranslation( panelWidth / 2, panelHeight / 2 );
 		star1 = at.createTransformedShape( star1 );
 		g2.setColor( Color.YELLOW );
@@ -81,17 +85,91 @@ class MyPanel extends JPanel{
 		
 		//Obstacles
 		//Wall - 1
+		wall_1 = new Rectangle2D.Double(-scale * 2, -scale * 6, scale * 4, scale * 12);
+		
 		at.setToTranslation( scale * 6, panelHeight - scale * 6 );
 		wall_1 = at.createTransformedShape( wall_1 );
 		g2.setColor( Color.BLUE );
 		g2.fill( wall_1 );
 		
 		//Wall - 2
-		at.setToTranslation( scale * 2, panelHeight - (panelHeight - scale * 2) );
+		wall_2 = new Rectangle2D.Double(-scale * 2, -scale * 4, scale * 4, scale * 8); //edit
+		
+		at.setToTranslation( scale * 2, scale * 4);
 		wall_2 = at.createTransformedShape( wall_2 );
 		g2.setColor( Color.BLUE );
 		g2.fill( wall_2 );
+				
+		//player
+		player = new Ellipse2D.Double( -scale, -scale, 2 * scale, 2 * scale );
+		
+		at.setToTranslation( translationX, translationY );
+		player = at.createTransformedShape( player );
+		g2.setColor( Color.RED );
+		g2.fill( player );
+	}
+
+	//Runnable
+	@Override
+	public void run() {
+		
+		while(true) {
+			
+			repaint();
+			translationX += vxPlayer;
+			translationY += vyPlayer;
+			
+			try {
+				Thread.sleep(100);
+			}catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		
 	}
 	
+	
+	
+	//Teclado - KeyListener
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+		int keyCode = e.getKeyCode();
+		
+		switch (keyCode) {
+		case KeyEvent.VK_UP:
+			vxPlayer = 0;
+			vyPlayer = -5;
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			vxPlayer = 5;
+			vyPlayer = 0;
+			break;
+			
+		case KeyEvent.VK_DOWN:
+			vxPlayer = 0;
+			vyPlayer = 5;
+			break;
+			
+		case KeyEvent.VK_LEFT:
+			vxPlayer = -5;
+			vyPlayer = 0;
+			break;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		vxPlayer = 0;
+		vyPlayer = 0;
+	}
+
 }
